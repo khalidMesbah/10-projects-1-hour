57eb6b4442978aa2a73acbd6dcc61067a49ab6ca
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-07-16 00:06:40 +0200

    finish toast & hamburger challenges

--- hamburger/script.js
+++ hamburger/script.js
@@ -1,7 +1,3 @@
-const btn = document.getElementById("btn");
-const nav = document.getElementById("nav");
-
-btn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-    btn.classList.toggle("active");
-});
+const btn = document.getElementById(`btn`);
+const nav = document.getElementById(`nav`);
+btn.addEventListener(`click`, () => (btn.classList.toggle(`active`), nav.classList.toggle(`active`)));
